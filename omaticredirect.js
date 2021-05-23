8622dcdf45955ba93501fdbe802cdd892abe2435
funcyber <84661382+funcyber@users.noreply.github.com>
2021-05-24 01:48:32 +0800

    Update omaticredirect.js

--- omaticredirect.js
+++ omaticredirect.js
@@ -8,10 +8,9 @@
         var ars      = "https://es.trivmvirate.com";
 	    var popbox = `<div class="popbox hide" id="popbox"><div aria-label='Close' class="pop-overlay" role="button" tabindex="0"/><div class="pop-content"><div class="popcontent" align="center"> <img src="https://1.bp.blogspot.com/-y8AsxfEerDc/YFSyMPZF14I/AAAAAAAAAAM/JUegMgSE-3o5A_06mx0Fir2-dkB6fAGvACLcBGAsYHQ/s640/re.jpg" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" width="640" height="320" alt="" /> <button class='g_url btn btn-success btn-dwn m-2'>Confirm</button> <br/></div> <button class='g_url popbox-close-button'>&times;</button></div></div>`;
 
-        if(!g_confirm && !go_ads.includes('#YOUR'))
-        {
+        
             $('#popbox').removeClass('hide');
-        }
+       
 
         $(document).on('click','.g_url',function(e)
         {
