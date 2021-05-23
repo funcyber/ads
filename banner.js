d6c49f818bc860ccb6a6fc2bb61a96e9a08a7c8d
funcyber <84661382+funcyber@users.noreply.github.com>
2021-05-23 23:59:09 +0800

    Add files via upload

---
+++ banner.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '645f10afb43ed327f949953504cd6a8f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplaycontent.com/645f10afb43ed327f949953504cd6a8f/invoke.js"></scr' + 'ipt>');
