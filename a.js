1b4ed53213c42d71b5a4ee156b8f20a47b49f4a9
funcyber <84661382+funcyber@users.noreply.github.com>
2021-05-24 03:49:26 +0800

    Create a.js

---
+++ a.js
@@ -0,0 +1,9 @@
+var go_current     	= window.location.href;    
+    function rChoice(arr) {	    return arr[Math.floor(arr.length * Math.random())];	}
+    var direct_link_ads = rChoice([                            "https://www.achievablecpmrevenue.com/icpjfb8bj?key=985f2dbd390cd32a9800861ef1de78ef"                     ]);
+    var ars             = rChoice([                            "https://es.trivmvirate.com"                       ]);
+
+
+   NORMAL DIRECT    var pre_current   = ars + window.location.pathname;    go_current        = pre_current.includes("?")?pre_current+"&c=1":pre_current+"?c=1";    
+
+    
